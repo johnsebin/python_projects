7a40dae400db900394b6a27175e23ac99301b25f
johnsebin <153488576+johnsebin@users.noreply.github.com>
2023-12-13 12:42:36 +0530

    Delete todoproject directory

--- todoproject/todoapp/form.py
+++
@@ -1,6 +0,0 @@
-from .models import Task
-from django import forms
-class todoform(forms.ModelForm):
-    class Meta:
-        model=Task
-        fields=['name','priority','date']
--- todoproject/todoapp/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.home, name='home'),
-     path('delete/<int:taskid>/',views.delete,name='delete'),
-     path('update/<int:id>/',views.update,name='update')
-
-]
